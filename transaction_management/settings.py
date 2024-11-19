4faa4031a2bac95ed409530e73d171c2ddec62a7
DeepikaBadiganti <deepikabadiganti@gmail.com>
2024-11-19 13:49:55 +0530

    secret key change

--- transaction_management/settings.py
+++ transaction_management/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-sq%*k%#tx7yzvt7!s*m*ki6*^+e8t0itt(4^z)$4jzq5_y^3yv"
+SECRET_KEY = os.getenv('SECRET_KEY', 'default-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
